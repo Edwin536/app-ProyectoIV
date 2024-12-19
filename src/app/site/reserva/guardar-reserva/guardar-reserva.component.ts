453b46d660b58ec3a84e8688831792a3b6310081
Geiner_FA <geinerfa@gmail.com>
2024-12-18 22:32:31 -0600

    reserva

--- src/app/site/reserva/guardar-reserva/guardar-reserva.component.ts
+++ src/app/site/reserva/guardar-reserva/guardar-reserva.component.ts
@@ -1,43 +1,55 @@
-
-import { AfterViewInit, Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import Swal from 'sweetalert2';
+import { Reserva } from '../../../models/Reserva';
 import { CommonModule } from '@angular/common';
-import { PaisService } from '../../../services/pais.service';
 import { Cliente } from '../../../models/Cliente';
+import { ClienteService } from '../../../services/cliente.service';
 import { ReservaService } from '../../../services/reserva.service';
 
 @Component({
   selector: 'app-guardar-reserva',
   imports: [RouterModule, ReactiveFormsModule, CommonModule],
   templateUrl: './guardar-reserva.component.html',
-  styleUrl: './guardar-reserva.component.css'
+  styleUrl: './guardar-reserva.component.css',
 })
-export class GuardarReservaComponent implements AfterViewInit {
+export class GuardarClienteComponent {
   // Variables del form.
   public cargando: boolean = false;
   public arrCliente: Cliente[] = [];
 
   // Definir los campos del formulario.
-  public nombre: FormControl;
-  public descripcion: FormControl;
-  public pais: FormControl;
-
-
+  public fecha_ida: FormControl;
+  public fecha_regreso: FormControl;
+  public precio: FormControl;
+  public estado: FormControl;
+  public cliente: FormControl;
 
   public formulario: FormGroup;
 
-  constructor(private servicio: ReservaService, private svCliente: PaisService, private router: Router) {
-    this.nombre = new FormControl('', Validators.required);
-    this.descripcion = new FormControl('', Validators.required);
-    this.pais = new FormControl('', Validators.required);
-    
+  constructor(
+    private servicio: ReservaService,
+    private svCliente: ClienteService,
+    private router: Router
+  ) {
+    this.fecha_ida = new FormControl('', Validators.required);
+    this.fecha_regreso = new FormControl('', Validators.required);
+    this.precio = new FormControl('', Validators.required);
+    this.estado = new FormControl(true, Validators.required);
+    this.cliente = new FormControl('', Validators.required);
 
     this.formulario = new FormGroup({
-      nombre: this.nombre,
-      descripcion: this.descripcion,
-      pais: this.pais,
+      fecha_ida: this.fecha_ida,
+      fecha_regreso: this.fecha_regreso,
+      precio: this.precio,
+      estado: this.estado,
+      cliente: this.cliente,
     });
   }
 
@@ -45,56 +57,58 @@ export class GuardarReservaComponent implements AfterViewInit {
     this.svCliente.Listar().subscribe({
       next: (resp: Cliente[]) => {
         this.arrCliente = resp;
-      }
+      },
     });
   }
 
   public Guardar(): void {
-    
     this.cargando = true;
 
-   
     const reserva = {
       idReserva: 0,
-      nombre: this.nombre.value,
-      descripcion: this.descripcion.value,
+      fecha_ida: this.fecha_ida.value,
+      fecha_regreso: this.fecha_regreso.value,
+      precio: this.precio.value,
       estado: true,
-      pais: {
-        idPais: this.pais.value
+      cliente: {
+        idCliente: this.cliente.value,
       } as Cliente
-       
-    } as Cliente;
+
+    } as Reserva;
 
     this.servicio.Guardar(reserva).subscribe({
       next: (resp: number) => {
         if (resp > 0) {
           Swal.fire({
-            title: "Aviso",
-            text: "Se guardó correctamente el reserva.",
-            icon: "success",
-            confirmButtonText: "Aceptar"
+            title: 'Aviso',
+            text: 'Se guardó correctamente el Reserva.',
+            icon: 'success',
+            confirmButtonText: 'Aceptar',
           });
         } else {
           Swal.fire({
-            title: "Aviso",
-            text: "No se pudo guardar el reserva, intente nuevamente.",
-            icon: "warning",
-            confirmButtonText: "Aceptar"
+            title: 'Aviso',
+            text: 'No se pudo guardar el Reserva, intente nuevamente.',
+            icon: 'warning',
+            confirmButtonText: 'Aceptar',
           });
         }
       },
       error: (e: any) => {
         Swal.fire({
-          title: "Aviso",
+          title: 'Aviso',
           text: `Ocurrió un error en el proceso, referencia de respuesta: ${e.message}`,
-          icon: "error",
-          confirmButtonText: "Aceptar"
+          icon: 'error',
+          confirmButtonText: 'Aceptar',
         });
       },
       complete: () => {
         this.cargando = false;
-        this.router.navigate(["/destinos"]);
-      }
+        this.router.navigate(['/reservas']);
+      },
     });
   }
+}
+function ngAfterViewInit() {
+  throw new Error('Function not implemented.');
 }
